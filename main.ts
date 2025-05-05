210fa6b1c9e1fd9a57c5ef24774921edd8dee1ff
JoaquinC539 <cjoaquinangel@gmail.com>
2025-05-05 12:09:58 -0600

    Initial commit

---
+++ main.ts
@@ -0,0 +1,15 @@
+import { app, BrowserWindow } from "electron";
+const path=require("path");
+
+function createWindow(){
+    const win:BrowserWindow=new BrowserWindow({
+        width:800,
+        height:600,
+        webPreferences:{
+            preload: path.join(__dirname,"preload.js"),
+            contextIsolation:true
+        }
+    });
+    win.loadFile("./index.html");
+}
+app.whenReady().then(createWindow)
